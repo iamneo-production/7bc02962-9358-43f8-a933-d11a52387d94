ab7978b60c15c1ee1618b7ac62f0a98ae4f2f4bb
SuprajaKrovvi <suprajakrovvidi2001@gmail.com>
2023-07-17 03:06:00 +0000

    entered 4th query

--- MYQUERIES.sql
+++ MYQUERIES.sql
@@ -9,3 +9,6 @@ select * from blood_bank where state='Andhra Pradesh';
 set timing on;
 select count(*) as totalcount from blood_bank;
 --4th query
+set timing on;
+--create index indname on blood_bank('Blood Bank Name');
+select "Blood Bank Name" from blood_bank order by "Blood Bank Name"fetch first 5 rows only;
