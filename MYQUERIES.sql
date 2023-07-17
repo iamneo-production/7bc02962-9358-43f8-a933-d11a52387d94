766a7b883d14aa996e0a59b716a966d3db4afd5f
SuprajaKrovvi <suprajakrovvidi2001@gmail.com>
2023-07-17 02:32:49 +0000

    entered third query

--- MYQUERIES.sql
+++ MYQUERIES.sql
@@ -5,3 +5,7 @@ select state,count(*) from blood_bank group by state;
 --2nd query
 set timing on;
 select * from blood_bank where state='Andhra Pradesh';
+--3rd query
+set timing on;
+select count(*) as totalcount from blood_bank;
+--4th query
