2a062c64807806f6bbe529672f05d8db6fb48eec
SuprajaKrovvi <suprajakrovvidi2001@gmail.com>
2023-07-17 02:25:07 +0000

    entered another query

--- MYQUERIES.sql
+++ MYQUERIES.sql
@@ -1,3 +1,7 @@
 set timing on;
+--1st query
 create index bloodbank_state on blood_bank(state);
 select state,count(*) from blood_bank group by state;
+--2nd query
+set timing on;
+select * from blood_bank where state='Andhra Pradesh';
