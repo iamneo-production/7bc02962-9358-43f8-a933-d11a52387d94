f4f8ef40d3110d459a86bc837efffd684ba78bb3
SuprajaKrovvi <suprajakrovvidi2001@gmail.com>
2023-07-30 08:59:58 +0000

    last commit

--- Supraja_krovvi.sql
+++ Supraja_krovvi.sql
@@ -1,17 +1,25 @@
-set timing on;
---1st query
-create index bloodbank_state on blood_bank(state);
-select state,count(*) from blood_bank group by state;
---2nd query
-set timing on;
-select * from blood_bank where state='Andhra Pradesh';
---3rd query
-set timing on;
-select count(*) as totalcount from blood_bank;
---4th query
-set timing on;
---create index indname on blood_bank('Blood Bank Name');
-select "Blood Bank Name" from blood_bank order by "Blood Bank Name"fetch first 5 rows only;
---5th query
-set timing on;
-select "Blood Bank Name" from blood_bank order by "Blood Bank Name";
+
+--query 1
+Set timing on;
+
+SELECT STATE, COUNT(*) AS Total_Blood_Banks_Count FROM BLOOD_BANK
+GROUP BY State;
+
+
+-- Q2. Write a SQL Query to List out the blood banks in Andhra Pradhes State?
+
+Set timing on;
+Select "Blood Bank Name" FROM BLOOD_BANK
+Where STATE = 'Andhra Pradesh';
+
+--query 3
+Set timing on;
+Select count(*) as totalbloodbanks from blood_bank;
+--query 4
+Set timing on;
+--Create index indexname on blood_bank('Blood Bank Name');
+Select "Blood Bank Name" from blood_bank order by "Blood Bank Name" asc 
+Fetch first 5 rows only;
+--query 5
+Set timing on;
+Select "Blood Bank Name" from blood_bank order by "Blood Bank Name" asc;
